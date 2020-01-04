bd9f8bba4fc4c05c739203fdb0aba81f724c89be
kumarrahul-rkv <rahulkumar.rkv@gmail.com>
2020-01-04 07:08:31 +0200

    Initial commit

---
+++ src/SalarySlipProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Kumarrahul\salarysliuploader;
+use Illuminate\Support\ServiceProvider;
+
+class SalarySlipProvider extends ServiceProvider {
+    
+    public function boot() {
+        /*$this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'rdashboardui');
+        $this->publishes([
+            __DIR__.'/assets' => public_path('assets'),
+            __DIR__.'/resources/views' => resource_path('views/kumarrahul/rdashboardui'),
+        ], 'rdashboardui');*/
+    }
+    
+    public function register() {
+        
+    }
+    
+}
